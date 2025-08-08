a8d174aa526ecff043646edf433352504a48063a
0xkrevX <166368579+0xkrevX@users.noreply.github.com>
2025-08-08 22:14:10 +0700

    Update features.py

--- utils/features.py
+++ utils/features.py
@@ -4,10 +4,11 @@ from construct import Struct as cStruct
 POOL_INFO_LAYOUT = cStruct(
     "instruction" / Int8ul, 
     "simulate_type" / Int8ul  
-)
+) 
 
 SWAP_LAYOUT = cStruct(
     "instruction" / Int8ul,
     "amount_in" / Int64ul,
     "min_amount_out" / Int64ul
 )
+
